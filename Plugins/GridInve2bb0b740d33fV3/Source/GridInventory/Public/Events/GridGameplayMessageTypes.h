22bf219107f5fa2d5c47ea9fbc91909b7c32d1e2
Max Heinze <maxheinze@googlemail.com>
2026-09-07 18:02:05 +0200

    [+] Add unversioned file to last commit, renamed GameplayMessageTypes2.h due to header name collision.

---
+++ Plugins/GridInve2bb0b740d33fV3/Source/GridInventory/Public/Events/GridGameplayMessageTypes.h
@@ -0,0 +1,51 @@
+﻿// Copyright 2025, Solar Corp. All right reserved
+// This file is based on Lyra example code
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "GridGameplayMessageTypes.generated.h"
+
+class UGameplayMessageRouter;
+
+// Match rule for message listeners
+UENUM(BlueprintType)
+enum class EGridGameplayMessageMatch : uint8
+{
+	// An exact match will only receive messages with exactly the same channel
+	// (e.g., registering for "A.B" will match a broadcast of A.B but not A.B.C)
+	ExactMatch,
+
+	// A partial match will receive any messages rooted in the same channel
+	// (e.g., registering for "A.B" will match a broadcast of A.B as well as A.B.C)
+	PartialMatch
+};
+
+/**
+ * Struct used to specify advanced behavior when registering a listener for gameplay messages
+ */
+template<typename FMessageStructType>
+struct FGridGameplayMessageListenerParams
+{
+	/** Whether Callback should be called for broadcasts of more derived channels or if it will only be called for exact matches. */
+	EGridGameplayMessageMatch MatchType = EGridGameplayMessageMatch::ExactMatch;
+
+	/** If bound this callback will trigger when a message is broadcast on the specified channel. */
+	TFunction<void(FGameplayTag, const FMessageStructType&)> OnMessageReceivedCallback;
+
+	/** Helper to bind weak member function to OnMessageReceivedCallback */
+	template<typename TOwner = UObject>
+	void SetMessageReceivedCallback(TOwner* Object, void(TOwner::* Function)(FGameplayTag, const FMessageStructType&))
+	{
+		TWeakObjectPtr<TOwner> WeakObject(Object);
+		OnMessageReceivedCallback = [WeakObject, Function](FGameplayTag Channel, const FMessageStructType& Payload)
+		{
+			if (TOwner* StrongObject = WeakObject.Get())
+			{
+				(StrongObject->*Function)(Channel, Payload);
+			}
+		};
+	}
+};
