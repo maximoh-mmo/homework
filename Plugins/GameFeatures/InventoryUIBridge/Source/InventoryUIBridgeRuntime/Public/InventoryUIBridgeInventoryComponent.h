de2502b4857dc949a8b6d7b0b373c6901db65d36
Max Heinze <maxheinze@googlemail.com>
2026-09-10 12:24:10 +0200

    Add InventoryUIBridge plugin: visual-only grid inventory mirror over Lyra authoritative inventory

---
+++ Plugins/GameFeatures/InventoryUIBridge/Source/InventoryUIBridgeRuntime/Public/InventoryUIBridgeInventoryComponent.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryComponent.h"
+#include "InventoryUIBridgeInventoryComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class INVENTORYUIBRIDGERUNTIME_API UInventoryUIBridgeInventoryComponent : public UInventoryComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UInventoryUIBridgeInventoryComponent();
+
+};
